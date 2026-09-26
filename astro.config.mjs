36c2de44773b5e45f576233979a4658f91521677
Kyle Lieber <klieber@users.noreply.github.com>
2026-09-25 21:29:41 -0500

    feat: add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://kylelieber.com',
   trailingSlash: 'always',
+
   build: {
     format: 'directory'
-  }
+  },
+
+  integrations: [sitemap()]
 });
